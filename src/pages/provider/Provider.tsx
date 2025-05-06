c915a5c9940c7ef9f00454e3fcd18e84c3fcbdec
theJRodrigues <gbrodrigues154+github@gmail.com>
2025-05-05 22:48:19 -0300

    feat: add Provider Page

---
+++ src/pages/provider/Provider.tsx
@@ -0,0 +1,16 @@
+const Provider = () => {
+  return (
+    <>
+      <header>
+        Provider component content
+      </header>
+      <main>
+        Provider component content
+      </main>
+      <footer>
+        Provider footer content
+      </footer>
+    </>
+  );
+};
+export default Provider;
