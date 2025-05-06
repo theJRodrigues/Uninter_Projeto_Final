ad412f8b3a9425eeebe15b405c3e46b345559986
theJRodrigues <gbrodrigues154+github@gmail.com>
2025-05-05 22:47:03 -0300

    refactoring: implemented button component to navigate to admin, patient and provider pages

--- src/pages/welcome/Welcome.tsx
+++ src/pages/welcome/Welcome.tsx
@@ -1,19 +1,51 @@
-const Welcome = () => {
+import { useNavigate } from "react-router";
+import Footer from "../../components/Footer";
+import ButtonToNavigate from "./components/ButtonToProviderPage";
+
+const Welcome = () => { 
+  const navigate = useNavigate();
+  
+  const navigateToPatientPage = () => {
+    navigate("/patient")
+  };
+  
+  const navigateToAdminPage = () => {
+    navigate("/admin")
+  };
+  
+  const navigateToProviderPage = () => {
+    navigate("/provider")
+  };
+  
   return (
-    <main className="bg-gradient-to-t from-blue-200 to-white min-h-svh font-serif text-gray-700 flex flex-col justify-center items-center">
+  <div className="min-h-svh flex flex-col">
+    <main className="grow bg-gradient-to-t from-blue-200 to-white font-serif text-gray-700 flex flex-col justify-center items-center">
       <section className="text-center space-y-3">
         <hgroup>
           <h1 className="text-blue-700 text-9xl font-bold">SGHSS</h1>
           <h2 className="text-xl text-blue-900">Sistema de Gestão Hospitalar Saúde e Segurança</h2>
         </hgroup>
         <p>Conectando pacientes e profissionais com agilidade</p>
-        <section className="space-x-5 text-white">
-          <button className="bg-green-600 py-2 px-3 rounded w-[200px] hover:bg-green-700 cursor-pointer"> Prestador</button>
-          <button className="bg-blue-600 py-2 px-3 rounded w-[200px] hover:bg-blue-700 cursor-pointer">Paciente</button>
-          <button className="bg-gray-600 py-2 px-3 rounded w-[200px] hover:bg-gray-700 cursor-pointer block m-auto mt-2">Administrador</button>
+        
+        <section className="space-x-5 text-white space-y-2">
+          <ButtonToNavigate 
+          handleClick={navigateToProviderPage} 
+          text="Prestador" />   
+           
+          <ButtonToNavigate 
+          handleClick={navigateToPatientPage} 
+          text="Paciente" 
+          className="bg-blue-600 hover:bg-blue-700"/>
+          
+          <ButtonToNavigate 
+          handleClick={navigateToAdminPage}
+          text="Administrador"
+          className="bg-gray-600 hover:bg-gray-700 block m-auto"/>
         </section>
       </section>
-    </main>
+    </main>  
+    <Footer />
+  </div>
   );
 };
 
