357fa65a811cc9cd5d98d0872d740fc9c5edc0db
theJRodrigues <gbrodrigues154+github@gmail.com>
2025-05-05 22:47:46 -0300

    feat: add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <footer className="bg-blue-800 text-center py-4 text-white relative">
+      <p>Jean Gabriel Rodrigues.</p>
+      <p>2025.</p>
+      <section>
+        <a 
+          href="https://github.com/theJRodrigues/Uninter_Projeto_Final" 
+          target='_blank'
+          className="absolute top-4 right-2"
+        >
+            Repositório Github
+        </a>
+      </section>
+    </footer>
+  );
+};
+
+export default Footer;
