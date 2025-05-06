29e9d819da8c5cfafb4282fec37566042fb18731
theJRodrigues <gbrodrigues154+github@gmail.com>
2025-05-05 22:43:07 -0300

    react-router routes implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { Route, Routes } from "react-router";
 import Welcome from "./pages/welcome/Welcome";
+import Provider from "./pages/provider/Provider";
 
 function App() {
-  return <Welcome />;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/patient" element={<Welcome />} />
+        <Route path="/provider" element={<Provider />} />
+        <Route path="/admin" element={<Welcome />} />
+      </Routes>
+    </>
+  )
 }
 
 export default App;
