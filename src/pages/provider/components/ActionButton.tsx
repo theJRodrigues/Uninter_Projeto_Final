3e1381d69205044c6664d7136144aedba1df4e6b
theJRodrigues <gbrodrigues154+github@gmail.com>
2025-05-06 23:43:00 -0300

    feat: created an action button component to choose between logging in and registering a provider

---
+++ src/pages/provider/components/ActionButton.tsx
@@ -0,0 +1,25 @@
+import { twMerge } from "tailwind-merge";
+
+interface ActionButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  action: () => void;
+  title: string;
+  text: string;
+}
+
+const ActionButton = ({action, title, text, ...props}: ActionButtonProps) => {
+  
+  const buttonClass = twMerge("bg-blue-600 py-1 px-4 rounded text-white cursor-pointer hover:bg-blue-700 w-[100%]", props.className);
+  return (
+    <div className=" space-y-1">
+      <h2 className="text-gray-600">
+        {title}
+      </h2>
+      <button className={buttonClass}
+        onClick={action}>
+        {text}
+      </button>
+    </div>
+  );
+};
+
+export default ActionButton;
