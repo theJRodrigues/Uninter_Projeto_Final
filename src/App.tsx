1b19f8b4697c8cbd45b543c4c41c096560eae218
theJRodrigues <gbrodrigues154+github@gmail.com>
2025-05-07 23:02:12 -0300

    add elemento to patient path route and set spacing in css

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { Route, Routes } from "react-router";
 import Welcome from "./pages/welcome/Welcome";
 import Provider from "./pages/provider/Provider";
+import Patient from "./pages/patient/Patient";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Welcome />} />
-        <Route path="/patient" element={<Welcome />} />
+        <Route path="/patient" element={<Patient />} />
         <Route path="/provider" element={<Provider />} />
         <Route path="/admin" element={<Welcome />} />
       </Routes>
